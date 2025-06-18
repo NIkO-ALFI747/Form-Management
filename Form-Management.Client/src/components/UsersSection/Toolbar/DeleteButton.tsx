aaacde690d23b43c9e5a621b64db13490f4f699b
Nikolay Ermilov <nikolayermilov774@gmail.com>
2025-06-18 12:55:37 +0500

    Implementing a client-server connection to delete multiple users

--- Form-Management.Client/src/components/UsersSection/Toolbar/DeleteButton.tsx
+++ Form-Management.Client/src/components/UsersSection/Toolbar/DeleteButton.tsx
@@ -1,6 +1,7 @@
 import { type FC } from 'react'
 import Button from 'react-bootstrap/Button'
 import type { User } from '../types/User'
+import { deleteUsers } from '../../../services/users'
 
 interface DeleteButtonProps {
   selectedCount: number
@@ -8,14 +9,19 @@ interface DeleteButtonProps {
   isDeleting: boolean
   setDeleting: React.Dispatch<React.SetStateAction<boolean>>
   isLoading: boolean
+  users: User[]
   setUsers: React.Dispatch<React.SetStateAction<User[]>>
 }
 
 const DeleteButton: FC<DeleteButtonProps> = ({ selectedCount, setSelectedCount,
-  isDeleting, setDeleting, isLoading, setUsers }) => {
+  isDeleting, setDeleting, isLoading, users, setUsers }) => {
 
-  const onDeleteUsers = () => {
+  const onDeleteUsers = async () => {
     setDeleting(true)
+    await deleteUsers(users
+      .filter(u => u.isSelected)
+      .map(u => u.id)
+    )
     setUsers(prevUsers => prevUsers.filter(prU => !prU.isSelected))
     setSelectedCount(0)
     setDeleting(false)
--- Form-Management.Client/src/components/UsersSection/Toolbar/Toolbar.tsx
+++ Form-Management.Client/src/components/UsersSection/Toolbar/Toolbar.tsx
@@ -5,21 +5,24 @@ import type { User } from '../types/User.tsx'
 
 interface ToolbarProps {
   isLoading: boolean
+  users: User[]
   setUsers: React.Dispatch<React.SetStateAction<User[]>>
   selectedCount: number
   setSelectedCount: React.Dispatch<React.SetStateAction<number>>
   loadUsers: () => Promise<void>
 }
 
-const Toolbar: FC<ToolbarProps> = ({ isLoading, setUsers, selectedCount, setSelectedCount, loadUsers }) => {
-  
+const Toolbar: FC<ToolbarProps> = ({ isLoading, users, setUsers,
+  selectedCount, setSelectedCount, loadUsers }) => {
+
   const [isDeleting, setDeleting] = useState<boolean>(false)
-  
+
   return (
     <div className="d-flex justify-content-between align-items-center mb-1">
       <LoadUsersButton isLoading={isLoading} loadUsers={loadUsers} isDeleting={isDeleting} />
       <DeleteButton selectedCount={selectedCount} setSelectedCount={setSelectedCount}
-        isDeleting={isDeleting} setDeleting={setDeleting} isLoading={isLoading} setUsers={setUsers}
+        isDeleting={isDeleting} setDeleting={setDeleting} isLoading={isLoading}
+        users={users} setUsers={setUsers}
       />
     </div>
   )
